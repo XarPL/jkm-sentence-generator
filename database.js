5a6567f40b3028f33e759b3e7e49d823e71b3393
Xardas49 <stormtrooper69pl@gmail.com>
2019-05-29 00:07:19 +0200

    add sentence generator and example data

---
+++ database.js
@@ -0,0 +1,55 @@
+
+	var w1 = [
+		"Ja chcę powiedzieć jedną rzecz: ",
+		"Trzeba powiedzieć jasno: ",
+		"Jak powiedział wybitny krakowianin Stanisław Lem ",
+		"Proszę mnie dobrze zrozumieć: ",
+		"Ja chciałem państwu przypomnieć że ",
+		"Niech państwo nie mają złudzeń: ",
+		"Powiedzmy to wyraźnie: "
+	];
+	var w2 = [
+		"przedstawiciele czerwonej hołoty ",
+		"ci wszyscy (tfu!) geje ",
+		"funkcjonariusze reżymowej telewizji ",
+		"tak zwani ekolodzy ",
+		"ci wszyscy (tfu!) demokraci ",
+		"agenci bezpieki ",
+		"feminazistki "
+	];
+	var w3 = [
+		"zupełnie bezkarnie ",
+		"całkowicie bezczelnie ",
+		"o poglądach na lewo od komunizmu ",
+		"celowo i świadomie ",
+		"z premedytacją ",
+		"od czasów Okrągłego Stołu ",
+		"w ramach postępu "
+	];
+	var w4 = [
+		"nawołują do podniesienia podatków ",
+		"próbują wyrzucić kierowców z miast ",
+		"próbują skłócić Polskę z Rosją ",
+		"głoszę brednię o globalnym ociepleniu ",
+		"zakazują posiadania broni ",
+		"nie dopuszczają prawicy do władzy ",
+		"uczą dzieci homoseksualizmu "
+	];
+	var w5 = [
+		"bo dzięki temu mogą kraść ",
+		"bo dostają za to pieniądze ",
+		"bo tak się uczy w państwowej szkole ",
+		"bo bez tego (tfu!) demokracja nie może istieć ",
+		"bo głupich jest więcej niż mądrych ",
+		"bo chcą tworzyć raj na ziemi ",
+		"bo chcą niszczyć cywilizację białego człowieka "
+	];
+	var w6 = [
+		"przez kolejne kadencje.",
+		"o czym się nie mówi.",
+		"i właśnie dlatego Europa umiera.",
+		"ale przyjdą muzułmanie i zrobią porządek.",
+		"- tak samo zresztą jak za Hitlera.",
+		"proszę zobaczyć, co się dzieje na Zachodzie, jeśli mi państwo nie wierzą.",
+		"co lat temu sto nikomu nie przyszłoby nawet do głowy."
+	];
---
+++ script.js
@@ -0,0 +1,8 @@
+﻿function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+function losu_start() 
+{
+	var sentence = document.getElementById("sentenceBox");
+        sentence.innerHTML = "&quot;"+w1[Math.floor(getRandomArbitrary(0, w1.length))] + w2[Math.floor(getRandomArbitrary(0, w2.length))] + w3[Math.floor(getRandomArbitrary(0, w3.length))] + w4[Math.floor(getRandomArbitrary(0, w4.length))] + w5[Math.floor(getRandomArbitrary(0, w5.length))] + w6[Math.floor(getRandomArbitrary(0, w6.length))] +"&quot;";
+}
